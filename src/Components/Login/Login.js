2b18f1cf3ce8fb13311f3a012a0d2f5db372bd80
Caleb Scoville <calebscoville2010@gmail.com>
2019-06-02 22:14:27 -0600

    Newest stuff

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,78 +1,128 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import axios from 'axios'
-import {updateUser} from '../../redux/userReducer'
+import { updateUser } from '../../redux/userReducer'
 import { connect } from 'react-redux'
 import RegisterForm from './RegisterForm'
 import LoginForm from './LoginForm'
 
 class Login extends Component {
-    constructor(){
-        super()
-        this.state = {
-            username: '',
-            password: ''
-        }
-    }
+	componentDidMount() {
+		axios.get('/auth/user').then((res) => {
+			this.props.updateUser(res.data)
+			this.props.history.push('/details')
+		})
+		this.props.id && this.props.history.push('/details')
+	}
 
-    componentDidMount(){
-        axios.get('/auth/user').then((res) => {
-            console.log(res.data)
-            this.props.updateUser(res.data)
-            this.props.history.push('/details')
-        })
-    }
-
-    handleLoginInfoUpdate = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
-
-    handleUserLogin = (e) => {
-        e.preventDefault()
-        const {username, password} = this.state
-        axios.post('/auth/login', {username, password}).then((res) => {
-            this.props.history.push('/details')
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-        e.target.username.value = ''
-        e.target.password.value = ''
-    }
-
-
-
-    render() {
-        console.log(this.state)
-        return (  
-        <div>
-            <h1>Login</h1>
-            <form onSubmit={this.handleUserLogin}>
-                <input 
-                type='text' 
-                name='username' 
-                placeholder='username'
-                onChange={this.handleLoginInforUpdate} 
-                />
-                <input 
-                type='password' 
-                name='password' 
-                placeholder='password' 
-                onChange={this.handleLoginInfoUpdate}
-                />
-                <button>Log In</button>
-            </form>
-        </div>
-        )
-    }
+	render() {
+		return (
+			<div>
+				<LoginForm />
+				<RegisterForm />
+			</div>
+		)
+	}
 }
 
 function mapStateToProps(reduxState) {
-    return reduxState
+	return reduxState
 }
 
 export default connect(
-    mapStateToProps,
-    { updateUser}
+	mapStateToProps,
+	{ updateUser }
 )(Login)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, {Component} from 'react'
+// import axios from 'axios'
+// import {updateUser} from '../../redux/userReducer'
+// import { connect } from 'react-redux'
+// import RegisterForm from './RegisterForm'
+// import LoginForm from './LoginForm'
+
+// class Login extends Component {
+//     constructor(){
+//         super()
+//         this.state = {
+//             username: '',
+//             password: ''
+//         }
+//     }
+
+//     componentDidMount(){
+//         axios.get('/auth/user').then((res) => {
+//             console.log(res.data)
+//             this.props.updateUser(res.data)
+//             this.props.history.push('/details')
+//         })
+//     }
+
+//     handleLoginInfoUpdate = (e) => {
+//         this.setState({
+//             [e.target.name]: e.target.value
+//         })
+//     }
+
+//     handleUserLogin = (e) => {
+//         e.preventDefault()
+//         const {username, password} = this.state
+//         axios.post('/auth/login', {username, password}).then((res) => {
+//             this.props.history.push('/details')
+//         })
+//         .catch((err) => {
+//             console.log(err)
+//         })
+//         e.target.username.value = ''
+//         e.target.password.value = ''
+//     }
+
+
+
+//     render() {
+//         console.log(this.state)
+//         return (  
+//         <div>
+//             <h1>Login</h1>
+//             <form onSubmit={this.handleUserLogin}>
+//                 <input 
+//                 type='text' 
+//                 name='username' 
+//                 placeholder='username'
+//                 onChange={this.handleLoginInforUpdate} 
+//                 />
+//                 <input 
+//                 type='password' 
+//                 name='password' 
+//                 placeholder='password' 
+//                 onChange={this.handleLoginInfoUpdate}
+//                 />
+//                 <button>Log In</button>
+//             </form>
+//         </div>
+//         )
+//     }
+// }
+
+// function mapStateToProps(reduxState) {
+//     return reduxState
+// }
+
+// export default connect(
+//     mapStateToProps,
+//     { updateUser}
+// )(Login)
